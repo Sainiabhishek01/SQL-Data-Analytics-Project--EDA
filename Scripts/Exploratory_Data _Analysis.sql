eb646f951b8b0c76ee98f3319ba1f754cb9d7061
Sainiabhishek01 <abhis6219@gmail.com>
2025-08-04 12:26:02 +0100

    Update Exploratory_Data _Analysis.sql

--- Scripts/Exploratory_Data _Analysis.sql
+++ Scripts/Exploratory_Data _Analysis.sql
@@ -1,1 +1,32 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To understand the structure of the database by listing all available tables.
+    - To review column details and metadata for specific tables.
 
+System Views Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+*/
+
+===============================================================================
+*/
+
+-- Retrieve a list of all tables in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
